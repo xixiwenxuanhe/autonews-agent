2cdb0712fe539f1082fd6827da868a2dc92f88f8
xixiwenxuanhe <xixiwenxuanhe@outlook.com>
2025-04-09 18:02:05 +0800

    修改端口为60005,开放IP+端口访问.

--- web/app.py
+++ web/app.py
@@ -144,5 +144,5 @@ def run_news_aggregation_task(hard=False, send=True):
 
 # 启动应用
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 60005))
     socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True) 
